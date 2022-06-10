21308e758a70953c16cb165d2b5a8ddfd5cb8f2e
ndvarn <45975432+ndvarn@users.noreply.github.com>
2022-06-10 12:09:45 +0300

    Make the script work, add updating.
    Fixed problems causing the script to not work, and implemented version updates.

--- Google Classroom User.user.js
+++ Google Classroom User.user.js
@@ -1,15 +1,21 @@
 // ==UserScript==
 // @name         Google Classroom User
 // @namespace    https://github.com/ndvarn/
-// @version      0.1
-// @description  Redirects to the correct user from the Google Classroom domain.
+// @version      0.2
+// @updateURL    https://raw.githubusercontent.com/ndvarn/Google-Classroom-User/main/Google%20Classroom%20User.user.js
+// @downloadURL  https://raw.githubusercontent.com/ndvarn/Google-Classroom-User/main/Google%20Classroom%20User.user.js
+// @description  Redirects to the correct user from the main user in Google Classroom.
 // @author       ndvarn
-// @match        https://classroom.google.com
+// @match        http://classroom.google.com/u/0/
+// @match        https://classroom.google.com/u/0/
+// @run-at       document-start
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    window.location.replace("/u/1/h/");
+    // If the account you use for Google Classroom is not the one you are redirected to,
+    // replace the "1" with the correct account number in the relative path below.
+    window.location.replace("../../u/1/");
 })();
