0507f9bb3cccb28ab117cd08028584eb97450669
ndvarn <45975432+ndvarn@users.noreply.github.com>
2022-06-10 11:03:15 +0300

    Add userscript
    Added the userscript to the repository

---
+++ Google Classroom User.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Google Classroom User
+// @namespace    https://github.com/ndvarn/
+// @version      0.1
+// @description  Redirects to the correct user from the Google Classroom domain.
+// @author       ndvarn
+// @match        https://classroom.google.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location.replace("/u/1/h/");
+})();
