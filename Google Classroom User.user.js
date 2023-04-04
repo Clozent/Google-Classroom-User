f7341e3aad059c845308366b08203e871c0d6d71
ndvarn <45975432+ndvarn@users.noreply.github.com>
2023-04-04 13:21:00 +0300

    Add a match pattern for better reliability
    Fix an issue where entering the host ("classroom.google.com") without being logged in with account 0 will instead take the user to the login page for account 0.
    This is because the user is redirected to a login page, instead of "classroom.google.com/u/0/".

--- Google Classroom User.user.js
+++ Google Classroom User.user.js
@@ -8,6 +8,8 @@
 // @author       ndvarn
 // @match        http://classroom.google.com/u/0/
 // @match        https://classroom.google.com/u/0/
+// @match        http://classroom.google.com/
+// @match        https://classroom.google.com/
 // @run-at       document-start
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
@@ -15,7 +17,14 @@
 
 (function() {
     'use strict';
+
+    console.log("Google Classroom UserScript is running");
     // If the account you use for Google Classroom is not the one you are redirected to,
     // replace the "1" with the correct account number in the relative path below.
-    window.location.replace("../../u/1/");
+    if (window.location.pathname[3] == '0') {
+        window.location.replace("../../u/1/");
+    }
+    else {
+        window.location.replace("/u/1/");
+    }
 })();
